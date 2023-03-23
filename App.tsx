4ffa37948817f89188e1723181715c240bf8c933
littxx <ederlanbrito@gmail.com>
2023-03-23 17:16:49 -0300

    first commit~

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, View, Text} from "react-native";
+import Button from "./components/Button";
+import Imagem from "./components/Imagem";
+import Display from "./components/Display";
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.font}>ola</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,  
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+  font:{
+    color: "#000000",
+    fontSize: 24,
+
+  }
+})
---
+++ components/Display.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native/types";
+
+
+ export default function Display(){
+    return(
+
+        <View>
+            <Text>ola mundo</Text>
+        </View>
+    
+}
+
